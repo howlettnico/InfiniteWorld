05bb76c19856b6604b4cff37dffe7eedccaab83a
FragrantWheel32 <howlettnico@gmail.com>
2026-06-23 00:00:32 -1000

    Rendering of all layers is now done by a single compute shader

--- Assets/Scripts/Utilities/Test.cs
+++ Assets/Scripts/Utilities/Test.cs
@@ -14,38 +14,38 @@ namespace Utilities
 
         private void Start()
         {
-            b = new SmartBitStream(new []{1, 8});
-
-            for (int i = 0; i < 100; i++)
-            {
-                int v = UnityEngine.Random.Range(0, 2);
-                b.AddNext(v);
-                vals.Add(v);
-                int v1 = UnityEngine.Random.Range(0, 128);
-                b.AddNext(v1);
-                vals1.Add(v1);
-            }
-
-            StringBuilder s = new StringBuilder();
-            for (int i = 0; i < 100; i++)
-            {
-                int v = b.ReadNext();
-                s.Append("Val: " + vals[i] + " Saved: " + v + (vals[i] == v ? " " : "WOTNOGNONGONGONG") + "\n");
-                int v1 = b.ReadNext();
-                s.Append("Val1: " + vals1[i] + " Saved1: " + v1 + (vals1[i] == v1 ? " " : "WOTNOGNONGONGONG") + "\n\n");
-            }
-
-            s.Append("NEXT Test: \n");
-            for (int i = 0; i < 100; i++)
-            {
-                b.GoToSegment(i);
-                int v = b.ReadNext();
-                s.Append("Val: " + vals[i] + " Saved: " + v + (vals[i] == v ? " " : "WOTNOGNONGONGONG") + "\n");
-                int v1 = b.ReadNext();
-                s.Append("Val1: " + vals1[i] + " Saved1: " + v1 + (vals1[i] == v1 ? " " : "WOTNOGNONGONGONG") + "\n\n");
-            }
-            
-            Debug.Log(s);
+            // b = new SmartBitStream(new []{1, 8});
+            //
+            // for (int i = 0; i < 100; i++)
+            // {
+            //     int v = UnityEngine.Random.Range(0, 2);
+            //     b.AddNext(v);
+            //     vals.Add(v);
+            //     int v1 = UnityEngine.Random.Range(0, 128);
+            //     b.AddNext(v1);
+            //     vals1.Add(v1);
+            // }
+            //
+            // StringBuilder s = new StringBuilder();
+            // for (int i = 0; i < 100; i++)
+            // {
+            //     int v = b.ReadNext();
+            //     s.Append("Val: " + vals[i] + " Saved: " + v + (vals[i] == v ? " " : "WOTNOGNONGONGONG") + "\n");
+            //     int v1 = b.ReadNext();
+            //     s.Append("Val1: " + vals1[i] + " Saved1: " + v1 + (vals1[i] == v1 ? " " : "WOTNOGNONGONGONG") + "\n\n");
+            // }
+            //
+            // s.Append("NEXT Test: \n");
+            // for (int i = 0; i < 100; i++)
+            // {
+            //     b.GoToSegment(i);
+            //     int v = b.ReadNext();
+            //     s.Append("Val: " + vals[i] + " Saved: " + v + (vals[i] == v ? " " : "WOTNOGNONGONGONG") + "\n");
+            //     int v1 = b.ReadNext();
+            //     s.Append("Val1: " + vals1[i] + " Saved1: " + v1 + (vals1[i] == v1 ? " " : "WOTNOGNONGONGONG") + "\n\n");
+            // }
+            //
+            // Debug.Log(s);
         }
 
         private void Update()
